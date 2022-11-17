595cd343759c8ae1896696733667fee140cafdcc
Shyju Krishnankutty <connectshyju@gmail.com>
2022-11-16 21:05:54 -0800

    boost::JSON parsing sample

--- SubModule1/main.cpp
+++ SubModule1/main.cpp
@@ -2,15 +2,38 @@
 //
 
 #include "main.h"
+#include <boost/json.hpp>
+#include <iostream>
+#include <fstream>
 
-using namespace std;
+using namespace boost::json;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	SPDLOG_INFO("Hello from SPDLOG");
 
-	SPDLOG_DEBUG("This is a debug message.");
-	SPDLOG_INFO("Hello Seattle.");
+    try
+    {
+        std::string dir("C:\\Dev\\site");
+        std::string fileName("worker.config.json");
+        std::string fullPath = dir + "/" + fileName;
+        
+        SPDLOG_INFO("fullPath:{}", fullPath);
 
+        std::ifstream ifs(fullPath);
+        std::string input(std::istreambuf_iterator<char>(ifs), {});
+
+        SPDLOG_INFO("Printing JSON:");
+        SPDLOG_INFO(input);
+
+        auto v = parse(input);
+        boost::json::object& o = v.as_object();
+        boost::json::object& d = o.at("description").as_object();
+    }
+    catch (const std::exception& ex) {
+        SPDLOG_ERROR("Caught unknown exception inside handler.{}", ex.what());
+    }
+
+    SPDLOG_WARN("Exiting main.");
 	return 0;
 }
