6479f4a3951791a9ef12c519420ada86f9b11375
Shyju Krishnankutty <connectshyju@gmail.com>
2022-11-16 19:28:38 -0800

    Add project files.

---
+++ SubModule1/main.cpp
@@ -0,0 +1,16 @@
+﻿// CMakeScratchPad.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	SPDLOG_DEBUG("This is a debug message.");
+	SPDLOG_INFO("Hello Seattle.");
+
+	return 0;
+}
---
+++ SubModule1/main.h
@@ -0,0 +1,10 @@
+﻿// CMakeScratchPad.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
