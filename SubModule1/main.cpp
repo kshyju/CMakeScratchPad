d9f183f56ee9009e7b05bce0429310b4bee74bac
Shyju Krishnankutty <connectshyju@gmail.com>
2022-11-19 18:27:00 -0800

    minimal app which runs for 5 seconds

--- SubModule1/main.cpp
+++ SubModule1/main.cpp
@@ -1,7 +1,6 @@
-﻿// CMakeScratchPad.cpp : Defines the entry point for the application.
-//
-
-#include "main.h"
+﻿#include "main.h"
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
@@ -11,6 +10,15 @@ int main()
 
 	SPDLOG_DEBUG("This is a debug message.");
 	SPDLOG_INFO("Hello Seattle.");
+
+	int counter = 1;
+
+	while (counter < 6)
+	{
+		std::cout << "Sleeping for 1 second." << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		counter++;
+	}
 
 	return 0;
 }
