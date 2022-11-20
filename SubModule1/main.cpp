0b9a99ab7a893fea729412d011f1e5c72422550c
Shyju Krishnankutty <connectshyju@gmail.com>
2022-11-19 18:19:51 -0800

    minimal app with CXX20

--- SubModule1/main.cpp
+++ SubModule1/main.cpp
@@ -2,6 +2,8 @@
 //
 
 #include "main.h"
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
@@ -11,6 +13,15 @@ int main()
 
 	SPDLOG_DEBUG("This is a debug message.");
 	SPDLOG_INFO("Hello Seattle.");
+
+	int counter = 1;
+
+	while (counter < 6)
+	{
+		std::cout << "Sleeping for 1 second." << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		counter++;
+	}
 
 	return 0;
 }
